ee45a445a9747d383f05a5c18e483f378e159220
Tyler Cady <tylercadyfairport@gmail.com>
2024-10-29 16:10:07 -0400

    authjs (5/beta) NOT nextauthjs

--- my-app/auth.config.ts
+++ my-app/auth.config.ts
@@ -1,4 +1,4 @@
 import Google from "next-auth/providers/google";
-import type { NextAuthConfig } from "next-auth";
+import { NextAuthConfig } from "next-auth";
 
 export default { providers: [Google] } satisfies NextAuthConfig;
