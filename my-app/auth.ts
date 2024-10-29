6dcce2ea0cae030e03dd81cba1d8d51fe5e21a73
Tyler Cady <tylercadyfairport@gmail.com>
2024-10-29 14:41:35 -0400

    depchange1

--- my-app/auth.ts
+++ my-app/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./lib/db";
-import authConfig from "@/auth.config";
+import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: MongoDBAdapter(clientPromise),
