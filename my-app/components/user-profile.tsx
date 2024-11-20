3f8d973abdfb214ee52b488f40a13d6cdac19a08
Tyler Cady <tylercadyfairport@gmail.com>
2024-11-19 22:24:51 -0500

    change loading back

--- my-app/components/user-profile.tsx
+++ my-app/components/user-profile.tsx
@@ -53,97 +53,79 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, name, email }) => {
     return (
         // do not change the first div at all
         <div className="flex items-center flex-col h-full w-full bg-w p-5 overflow-y-auto gap-4">
-            {!isLoading && (
-                <>
-                    {/*Profile Section*/}
-                    <div className="w-3/4 h-auto p-5 bg-white rounded-md shadow flex-col gap-4 flex">
-                        <div className="justify-between items-center gap-7 flex">
-                            <div className="text-blue text-2xl font-bold">
-                                My Profile
-                            </div>
-                            <div className="flex items-center gap-2 cursor-pointer">
-                                <Button
-                                    icon={<EditIcon />}
-                                    text="Edit"
-                                    type="secondary"
-                                    onClick={() => "/"}
-                                >
-                                    Edit
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="break-all">
-                            <div className="text-black text-2xl font-bold">
-                                {name}
-                            </div>
-                            <div className="text-gray text-xl font-normal">
-                                {email}
-                            </div>
-                        </div>
-                        <div className="break-all">
-                            <div className="text-black text-xl font-bold">
-                                Role
-                            </div>
-                            <div className="text-gray text-base font-normal">
-                                Parent
-                            </div>
-                        </div>
-                        {userFormData && (
-                            <div>
-                                <p>
-                                    Number children: {userFormData.numChildren}
-                                </p>
-                                <p>Car capacity: {userFormData.carCapacity}</p>
-                            </div>
-                        )}
+            {/*Profile Section*/}
+            <div className="w-3/4 h-auto p-5 bg-white rounded-md shadow flex-col gap-4 flex">
+                <div className="justify-between items-center gap-7 flex">
+                    <div className="text-blue text-2xl font-bold">
+                        My Profile
                     </div>
-                    {/*Family Section*/}
-                    <div className="w-3/4 h-auto p-5 bg-white rounded-md shadow flex-col gap-4 flex">
-                        <div className="justify-between items-center gap-7 flex">
-                            <div className="text-blue text-2xl font-bold">
-                                Family
-                            </div>
-                            <div className="flex items-center gap-2 cursor-pointer">
-                                <Button
-                                    icon={<AddIcon />}
-                                    text="Add"
-                                    type="secondary"
-                                    onClick={() => "/"}
-                                >
-                                    Add
-                                </Button>
-                            </div>
+                    <div className="flex items-center gap-2 cursor-pointer">
+                        <Button
+                            icon={<EditIcon />}
+                            text="Edit"
+                            type="secondary"
+                            onClick={() => "/"}
+                        >
+                            Edit
+                        </Button>
+                    </div>
+                </div>
+                <div className="break-all">
+                    <div className="text-black text-2xl font-bold">{name}</div>
+                    <div className="text-gray text-xl font-normal">{email}</div>
+                </div>
+                <div className="break-all">
+                    <div className="text-black text-xl font-bold">Role</div>
+                    <div className="text-gray text-base font-normal">
+                        Parent
+                    </div>
+                </div>
+                {userFormData && (
+                    <div>
+                        <p>Number children: {userFormData.numChildren}</p>
+                        <p>Car capacity: {userFormData.carCapacity}</p>
+                    </div>
+                )}
+            </div>
+            {/*Family Section*/}
+            <div className="w-3/4 h-auto p-5 bg-white rounded-md shadow flex-col gap-4 flex">
+                <div className="justify-between items-center gap-7 flex">
+                    <div className="text-blue text-2xl font-bold">Family</div>
+                    <div className="flex items-center gap-2 cursor-pointer">
+                        <Button
+                            icon={<AddIcon />}
+                            text="Add"
+                            type="secondary"
+                            onClick={() => "/"}
+                        >
+                            Add
+                        </Button>
+                    </div>
+                </div>
+                {/*no children info has been converted to vhvw bc I'll need to take out this section anyway after we fix the form */}
+                <div>
+                    <div className="text-black text-2xl font-bold">Child A</div>
+                    <div className="flex-col justify-start items-start gap-2.5">
+                        <div className="text-gray text-xl font-normal">
+                            Centerville Choir Group
                         </div>
-                        {/*no children info has been converted to vhvw bc I'll need to take out this section anyway after we fix the form */}
-                        <div>
-                            <div className="text-black text-2xl font-bold">
-                                Child A
-                            </div>
-                            <div className="flex-col justify-start items-start gap-2.5">
-                                <div className="text-gray text-xl font-normal">
-                                    Centerville Choir Group
-                                </div>
-                                <div className="text-gray text-xl font-normal">
-                                    Fairport High Cross Country
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="text-black text-2xl font-bold">
-                                Child B
-                            </div>
+                        <div className="text-gray text-xl font-normal">
+                            Fairport High Cross Country
                         </div>
                     </div>
-                    {/*Availability Section*/}
-                    <div className="w-3/4 h-auto p-5 bg-white rounded-md shadow flex-col gap-4 flex">
-                        <div className="flex-col justify-start items-start flex">
-                            <div className="text-blue text-2xl font-bold">
-                                Driving Availability
-                            </div>
-                        </div>
+                </div>
+                <div>
+                    <div className="text-black text-2xl font-bold">Child B</div>
+                </div>
+            </div>
+            {/*Availability Section*/}
+            <div className="w-3/4 h-auto p-5 bg-white rounded-md shadow flex-col gap-4 flex">
+                <div className="flex-col justify-start items-start flex">
+                    <div className="text-blue text-2xl font-bold">
+                        Driving Availability
                     </div>
-                </>
-            )}
+                </div>
+            </div>
         </div>
     );
 };
