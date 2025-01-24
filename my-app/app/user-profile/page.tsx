bee09bb7b5c7d344c1ec7fdc9e37df97aa70bfdc
mattdworkin <mtdworkin@gmail.com>
2025-01-24 10:38:14 -0500

    fixing carpools and profile header visibility

--- my-app/app/user-profile/page.tsx
+++ my-app/app/user-profile/page.tsx
@@ -4,12 +4,25 @@ import Header from "@/components/header";
 //import Login from "@/components/login";
 import Main from "@/components/main";
 import UserProfile from "@/components/user-profile";
+import { checkFormCompletion } from "@/lib/user-form-data";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
     const session = await auth();
+    const userId = session?.user?.id;
+
+    let isFormComplete = false;
+    
+    if (userId) {
+        isFormComplete = await checkFormCompletion(userId);
+        if (!isFormComplete) {
+            redirect("/user-form"); // Redirect to form if incomplete
+        }
+    }
+
     return (
         <div className="flex items-center justify-between h-screen flex-col bg-w">
-            <Header userId={session?.user?.id} />
+            <Header userId={userId} isFormComplete={isFormComplete} />
             {session?.user?.id ? (
                 <UserProfile
                     userId={session?.user?.id}
