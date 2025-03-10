1880e7e162ffcc458f71963de7f2dbe0ba0c7947
Tyler Cady <tylercadyfairport@gmail.com>
2025-03-10 00:18:47 -0400

    deploy changes

--- my-app/app/pool-info/[index]/page.tsx
+++ my-app/app/pool-info/[index]/page.tsx
@@ -4,20 +4,26 @@ import Header from "@/components/header";
 import PoolInfo from "@/components/pool-info";
 import Main from "@/components/main";
 
-export default async function DynamicInfo({ params }: { params: { index: string } }) {
-  const session = await auth();
-  const { index } = await Promise.resolve(params);
-  return (
-    <div className="h-full flex items-center justify-between flex-col bg-w">
-        <Header userId={session?.user?.id} 
-        isFormComplete={true}
-        currentPath={`/pool-info/${index}`} />
-        {session?.user?.id ? (
-            <PoolInfo userId={session?.user?.id} index={index} />
-        ) : (
-            <Main userName={session?.user?.name} />
-        )}
-        <Footer />
-    </div>
-  );
+export default async function DynamicInfo({
+    params,
+}: {
+    params: Promise<{ index: string }>;
+}) {
+    const session = await auth();
+    const { index } = await Promise.resolve(params);
+    return (
+        <div className="h-full flex items-center justify-between flex-col bg-w">
+            <Header
+                userId={session?.user?.id}
+                isFormComplete={true}
+                currentPath={`/pool-info/${index}`}
+            />
+            {session?.user?.id ? (
+                <PoolInfo userId={session?.user?.id} index={index} />
+            ) : (
+                <Main userName={session?.user?.name} />
+            )}
+            <Footer />
+        </div>
+    );
 }
