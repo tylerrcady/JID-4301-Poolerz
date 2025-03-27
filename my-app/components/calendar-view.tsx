a4165837ccee30e63c13162a349e02d60a52662d
Narayanan, Natasha Maya <mayanarayanan04@gmail.com>
2025-03-27 18:08:10 -0400

    passing in proper values

--- my-app/components/calendar-view.tsx
+++ my-app/components/calendar-view.tsx
@@ -11,11 +11,19 @@ interface CalendarViewProps {
   userId: string | undefined;
 }
 
+// created to match hardcoded event
 interface CalendarEvent {
   title: string;
   start: Date;
   end: Date;
   color?: string;
+  rRule: string,
+}
+
+// carpool calendar event
+interface CarpoolCalendarEvent {
+  title: string;
+  day: Number;
 }
 
 // Helper function to add days to a date
@@ -27,8 +35,8 @@ const addDays = (date: Date, days: number): Date => {
 
 const CalendarView: React.FC<CalendarViewProps> = ({ userId }) => {
   // Set default date to the current date
+  userId = "67d3358e45ee3c027f8e59ce"; // DELETE line later, will use for testing rn
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
-  const [results, setResults] = useState<TransformedResults[]>([]); // carpools with optimized results
 
   // Get Handler for receiving all the carpools from user-carpool-data
   const handleUserDataGet = useCallback(async () => {
@@ -47,6 +55,7 @@ const CalendarView: React.FC<CalendarViewProps> = ({ userId }) => {
             const foundCarpools = data?.createCarpoolData.userData.carpools || [];
             // extract the carpoolIds
             const filterCarpoolIds: string[] = foundCarpools.map((cp: any) => cp.carpoolId);
+            console.log(filterCarpoolIds);
             return filterCarpoolIds;
         } else {
             console.error("Failed to fetch data:", response.statusText);
@@ -71,7 +80,7 @@ const CalendarView: React.FC<CalendarViewProps> = ({ userId }) => {
             );
             if (response.ok) {
                 const data = await response.json();
-                console.log(data?.results); // Return the fetched data
+                //console.log(data?.results); // Return the fetched data
                 if (!data?.results) {
                   return null;
                 }  
@@ -90,20 +99,55 @@ const CalendarView: React.FC<CalendarViewProps> = ({ userId }) => {
 
   // Method coordinates receiving the carpools with optimization results
   const fetchCarpoolsWithOpt = useCallback(async () => {
-    const carpoolIds = await handleUserDataGet(); // this will receive all the carpoolsIds belonging to user found in user-carpool-data
-    console.log(carpoolIds);
-    carpoolIds?.forEach(async (carpoolId) => {
+    const carpoolIds = await handleUserDataGet();
+
+    if (!carpoolIds || carpoolIds.length === 0) {
+      return;
+    }
+
+    // Filter so that only carpoolIds with opt results remain
+    const carpoolPromises = carpoolIds.map(async (carpoolId) => {
       const result = await checkCarpoolWithOptResults(carpoolId);
-      if (result) {
-        results.push(result);
-      }
-    })
-    console.log(results);
+      return result || null;
+    });
+
+    const resolved = await Promise.all(carpoolPromises);
+
+    const finalCarpools = resolved.filter(
+      (res): res is TransformedResults => res !== null
+    );
+
+    return finalCarpools;
   }, []);
 
-  // Handler receives all carpools with optimization results and updates results variable accordingly
+  // Helper method that fetches driving schedules from user's carpool groups
+  const receiveDrivingSchedules = (results: TransformedResults[] | undefined): Record<string, string>[] | null => {
+    const schedules: Record<string, string>[] = [];
+    console.log("Carpools we are extracting from: ", results);
+    for (const result of results || []) {
+      for (const carpool of result.carpools || []) {
+        const userIsInCarpool = carpool.members?.some(
+          (memberPair) => memberPair[0] === userId
+        );
+    
+        if (userIsInCarpool) {
+          schedules.push(carpool.driverSchedule);
+          break; // move on to next result after finding the first match
+        }
+      }
+    }
+    console.log("Driving Schedules: ", schedules);
+    return schedules;
+  }
+
+  // Handler receives the driving schedules
   useEffect(() => {
-    fetchCarpoolsWithOpt();
+    const receiveEvents = async () => {
+      const finalCarpools = await fetchCarpoolsWithOpt();
+      const schedules = receiveDrivingSchedules(finalCarpools); // consider making results a local non-State variable
+    };
+
+    receiveEvents();
   }, []);
 
 
@@ -111,15 +155,17 @@ const CalendarView: React.FC<CalendarViewProps> = ({ userId }) => {
   const events: CalendarEvent[] = [
     {
       title: "SkyZone",
-      start: new Date(2025, 2, 29, 20, 0),
-      end: new Date(2025, 2, 29, 22, 0),
+      start: new Date(2025, 2, 28, 20, 0),
+      end: new Date(2025, 2, 28, 22, 0),
       color: "orange",
+      rRule: 'RRULE:FREQ=WEEKLY;BYDAY=FR',
     },
     {
       title: "Lunch",
-      start: new Date(2025, 2, 31, 12, 0),
-      end: new Date(2025, 2, 31, 13, 0),
+      start: new Date(2025, 2, 26, 12, 0),
+      end: new Date(2025, 2, 26, 13, 0),
       color: "skyblue",
+      rRule: 'RRULE:FREQ=WEEKLY;BYDAY=WE',
     },
     // Add more events here...
   ];
